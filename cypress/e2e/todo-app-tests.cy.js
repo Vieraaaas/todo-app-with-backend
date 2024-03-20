7ff7abac50b49b00bc223ec9e95f83a16fd65e60
Claas Paaschburg <paaschburgc@gmail.com>
2024-03-20 14:23:14 +0100

    test: test filter feature

--- cypress/e2e/todo-app-tests.cy.js
+++ cypress/e2e/todo-app-tests.cy.js
@@ -6,6 +6,8 @@ describe("Todo App", () => {
 
   beforeEach(() => {
     cy.visit("http://localhost:3000");
+    //cy.request fetches the data from the backend and prevents issues with asynchronicity
+    cy.request("http://localhost:4730/todos/");
   });
 
   it("has an input field", () => {
@@ -17,5 +19,24 @@ describe("Todo App", () => {
     cy.get("[data-cy=list]").children().last().should("have.text", task1);
     cy.get("[data-cy=input]").type(`${task2}{enter}`);
     cy.get("[data-cy=list]").children().last().should("have.text", task2);
+  });
+
+  it("filters finished tasks", () => {
+    cy.contains(`${task1}`).parent().find("input[type=checkbox]").check();
+    cy.get("[data-cy=radio-done").click();
+    cy.contains(`${task1}`).should("not.be.hidden");
+    cy.contains(`${task2}`).should("be.hidden");
+  });
+
+  it("filters open tasks", () => {
+    cy.get("[data-cy=radio-open").click();
+    cy.contains(`${task1}`).should("be.hidden");
+    cy.contains(`${task2}`).should("not.be.hidden");
+  });
+
+  it("reverts filters", () => {
+    cy.get("[data-cy=radio-all").click();
+    cy.contains(`${task1}`).should("not.be.hidden");
+    cy.contains(`${task2}`).should("not.be.hidden");
   });
 });
